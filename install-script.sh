e1cdb9b942c67b01375ad74ed6a56bfed9e417b8
MKHenson <mat@webinate.net>
2015-06-04 23:21:25 +0100

    Added installer and updated version

---
+++ install-script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+{ # this ensures the entire script is downloaded #
+
+# Stops the execution of a script if a command or pipeline has an error
+set -e
+
+# Functiom that prints the latest stable version
+version() {
+  echo "v0.0.1"
+}
+
+echo "Downloading latest version from github $(version)"
+
+#download latest
+wget https://github.com/MKHenson/webinate-blacktip/archive/master.zip
+unzip -o "master.zip" "webinate-blacktip-master/bin/*"
+
+# Moves the server folder to the current directory
+mv webinate-blacktip-master/bin/* .
+
+# Remove modepress-master
+if [ -d "webinate-blacktip-master" ]; then
+	rm webinate-blacktip-master -R
+fi
+
+# Remove the zip file
+rm master.zip
+
+# All done
+echo "Blacktip successfully installed :)"
+exit
+} # this ensures the entire script is downloaded #
